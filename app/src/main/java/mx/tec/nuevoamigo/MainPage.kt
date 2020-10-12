9c3d18220bd1e929aeda9baa42dde4f645873b10
LiraNeon <55103410+LiraNeon@users.noreply.github.com>
2020-10-11 23:29:21 -0500

    MainPage nearly 100% done

---
+++ app/src/main/java/mx/tec/nuevoamigo/MainPage.kt
@@ -0,0 +1,55 @@
+package mx.tec.nuevoamigo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MainPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+
+        var spinner1Selected = 0
+        val spinnerOpciones = findViewById<Spinner>(R.id.spinnerHeight)
+
+        ArrayAdapter.createFromResource(this,R.array.opcionesTamaño,android.R.layout.simple_spinner_item).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerOpciones.adapter = adapter
+        }
+
+        spinnerOpciones.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                spinner1Selected = p2
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+
+        var spinner2Selected = 0
+        val spinnerOpciones2 = findViewById<Spinner>(R.id.spinnerSex)
+
+        ArrayAdapter.createFromResource(this,R.array.opcionesSexo,android.R.layout.simple_spinner_item).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerOpciones2.adapter = adapter
+        }
+
+        spinnerOpciones2.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                spinner2Selected = p2
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+
+    }
+
+
+
+}
