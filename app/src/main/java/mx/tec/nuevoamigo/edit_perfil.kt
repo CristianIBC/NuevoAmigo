f198d15cddf54f77530feb7566283133bf77635c
fernando <a01422285@itesm.mx>
2020-10-12 22:18:44 -0500

    Layout de completar los datos

---
+++ app/src/main/java/mx/tec/nuevoamigo/edit_perfil.kt
@@ -0,0 +1,11 @@
+package mx.tec.nuevoamigo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class edit_perfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_perfil)
+    }
+}
