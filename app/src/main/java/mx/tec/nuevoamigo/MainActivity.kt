f57d0e1adecffe546ff528e1d73e4fca7b1bd983
LiraNeon <55103410+LiraNeon@users.noreply.github.com>
2020-10-24 13:04:01 -0500

    imagenes

--- app/src/main/java/mx/tec/nuevoamigo/MainActivity.kt
+++ app/src/main/java/mx/tec/nuevoamigo/MainActivity.kt
@@ -11,7 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //LINEA DE PRUEBA
-
         imgLogo.setOnClickListener {
             var i = Intent(this@MainActivity, edit_perfil::class.java)
             startActivity(i)
