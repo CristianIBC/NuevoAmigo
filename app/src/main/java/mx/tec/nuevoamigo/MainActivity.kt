7cbcb843f98acc7188e91eec11e7fc731ad03348
LiraNeon <55103410+LiraNeon@users.noreply.github.com>
2020-11-11 23:30:40 -0600

    correo en boton correcto

--- app/src/main/java/mx/tec/nuevoamigo/MainActivity.kt
+++ app/src/main/java/mx/tec/nuevoamigo/MainActivity.kt
@@ -47,9 +47,6 @@ enum class ProviderType{
 
 
 class MainActivity : AppCompatActivity() {
-    //MAIL SENDER
-    lateinit var appExecutors: AppExecutors
-
     val PERMISSION_ID = 1010
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     lateinit var locationRequest: LocationRequest
@@ -59,9 +56,6 @@ class MainActivity : AppCompatActivity() {
     private val callbackManager = CallbackManager.Factory.create()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        //MAIL VARIABLE
-        appExecutors = AppExecutors()
-        //MAIL VARIABLE END
 
         Thread.sleep(2000)
         setTheme(R.style.AppTheme_NoActionBar)
@@ -106,11 +100,6 @@ class MainActivity : AppCompatActivity() {
         getLastLocation()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btnMail.setOnClickListener {
-            sendEmail()
-        }
-
         //verficaSession()
 
 
@@ -437,51 +426,6 @@ class MainActivity : AppCompatActivity() {
         return cityName
     }
 
-    //EMAIL
-    private fun sendEmail(){
-        val cred= Credentials()
-        appExecutors.diskIO().execute {
-            val props = System.getProperties()
-            props.put("mail.smtp.host", "smtp.gmail.com")
-            props.put("mail.smtp.socketFactory.port", "465")
-            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
-            props.put("mail.smtp.auth", "true")
-            props.put("mail.smtp.port", "465")
-
-            val session =  Session.getInstance(props,
-                object : javax.mail.Authenticator() {
-                    //Authenticating the password
-                    override fun getPasswordAuthentication(): PasswordAuthentication {
-                        return PasswordAuthentication(cred.correo, cred.contra)
-                    }
-                })
-
-            try {
-                //Creating MimeMessage object
-                val mm = MimeMessage(session)
-                //Setting sender address
-                mm.setFrom(InternetAddress(cred.correo))
-                //Adding receiver
-                mm.addRecipient(
-                    Message.RecipientType.TO,
-                    InternetAddress("juanchino123@gmail.com"))
-                //Adding subject
-                mm.subject = "Adoptar perrito"
-                //Adding message
-                mm.setText("HOLA MI PANA")
-
-                //Sending email
-                Transport.send(mm)
-
-                appExecutors.mainThread().execute {
-                    //Something that should be executed on main thread.
-                }
-
-            } catch (e: MessagingException) {
-                e.printStackTrace()
-            }
-        }
-    }
 
 }
 
